8c872f273fb67bacd5e813f64b7c1f8d59bb2f68
guy793 <guy793@gmail.com>
2021-09-26 17:08:45 +0300

    update files to use tensor board instead of plotting

--- Train_thirdNet.py
+++ Train_thirdNet.py
@@ -5,48 +5,13 @@ from DataLoader import *
 import torch
 from torchvision import datasets, transforms as T
 from MythirdNet import LSTM_4th
+from My4thNet import *
 from IPython.display import clear_output
 import torchvision.models as models
+from torch.utils.tensorboard import SummaryWriter
 
-def live_plot(iteration,Loss,Estimated_range,Gt_range ,figsize=(7,5), title=''):
-  clear_output(wait=True)
-  plt.subplot(2, 2, 1)
-  plt.plot(iteration, Loss)
-  plt.tight_layout()
-  plt.xlabel('epoch')
-  plt.ylabel('Loss')
-  plt.subplot(2, 2, 2)
-  plt.plot(iteration, Estimated_range,label='Esitmated range [m]')
-  plt.plot(iteration,Gt_range,label='Predicted Range[m]')
-  plt.legend()
-  plt.tight_layout()
-  plt.xlabel('epoch')
-  plt.ylabel('Range[m]')
-  plt.pause(0.01) 
-  plt.show()
 
-def my_plotter(fig,ax1,ax2,Iteration_numer=[],estimated_range=[],Ground_truth_range=[],Loss=[]):
-    ax1.clear()
-    ax2.clear()
-    clear_output(wait=True)
-    # plt.figure(figsize=figsize)
-    # ax1.plot(Iteration_numer, estimated_range[3], label='prediction')
-    # ax1.plot(Iteration_numer, Ground_truth_range[3],'--b', label='Ground Truth')
-    # ax1.legend(loc ="lower left")
-    # ax1.legend()
-    # ax1.set_xlabel('iterations')
-    # ax1.set_ylabel('range[m]')
-    # ax1.set_title("Train plotter")
-    ax2.plot(Iteration_numer,Loss)
-    ax2.set_xlabel('iterations')
-    ax2.set_ylabel('Loss value')
-    # ax2.set_title("shares y with main")
-    fig.tight_layout()
-    plt.pause(0.1)
-    plt.show()
-plt.ion() # enable interactivity
-fig, (ax1, ax2) = plt.subplots(1,2)
-fig.suptitle('Vertically stacked subplots')
+writer = SummaryWriter()
 transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor()])
 #aquiring dataset and dataLoader
 
@@ -55,9 +20,9 @@ img_dir = r'C:\Users\Study\Desktop\airborne-detection-starter-kit\data\part1\Ima
 flight_dir = r'C:\Users\Study\Desktop\airborne-detection-starter-kit\data\part1\Images\*'
 
 #if using shali computer
-annotations_file_path=r'C:\Users\shali\part1\ImageSets\groundtruth.json'
-img_dir=r'C:\Users\shali\part1\Images\\'
-flight_dir=r'C:\Users\shali\part1\Images\*'
+# annotations_file_path=r'C:\Users\shali\part1\ImageSets\groundtruth.json'
+# img_dir=r'C:\Users\shali\part1\Images\\'
+# flight_dir=r'C:\Users\shali\part1\Images\*'
 
 flight_ids=check_available_flights(flight_dir)
 print(flight_ids)
@@ -72,13 +37,16 @@ vgg16 = models.vgg16(pretrained=True)
 vgg16.eval()
 #define network parameters
 num_epochs = 2000
-learning_rate = 0.001
+learning_rate = 0.1
 input_size = 1
 hidden_size = 2
 num_layers = 1
 num_classes = 1
 
+#choose regression model
 Regression_model=LSTM_4th()
+
+Regression_model=Net_4()
 Regression_model.train()
 criterion = torch.nn.MSELoss()  # mean-squared error for regression
 optimizer = torch.optim.Adam(Regression_model.parameters(), lr=learning_rate)
@@ -92,7 +60,6 @@ line1 = []
 line2 = []
 prediction_list=[]
 ground_truth_list=[]
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 vgg16.eval().to(device)
 Regression_model.to(device)
@@ -113,15 +80,20 @@ for epoch in range(num_epochs):
         loss1 = criterion(outputs.squeeze().to(device),(Range.squeeze().to(device)).float())   
         loss=loss1   
         y1_data.append(loss.item())
-        prediction_list.append(outputs.squeeze().detach().cpu().numpy())
-        ground_truth_list.append(Range.squeeze().detach().cpu().numpy())
+        writer.add_scalar('Loss/train', loss.item(), iter)
 
+        prediction_list.append(outputs.squeeze()[3].item())
+        ground_truth_list.append(Range.squeeze()[3].item())
+
+        writer.add_scalars('Prediction vs Ground Truth', {'Prediction': prediction_list[-1],
+                                       'Ground Truth': ground_truth_list[-1]}, iter)
+        # print(loss.item())
         loss.backward()
         optimizer.step()
-        my_plotter(fig, ax1, ax2, Iteration_numer=iteration, estimated_range=prediction_list,
-                   Ground_truth_range=ground_truth_list, Loss=y1_data)
+
     if epoch % 5 == 1 and epoch>0 :
         Regression_model.save_model(Regression_model)
-        # live_plot(iteration,y1_data,prediction_list,ground_truth_list)
+
 
         # print('accuracy range %',(np.abs(outputs.squeeze().item()-Range.squeeze().item())/Range.squeeze().item())*100)
+writer.close()
